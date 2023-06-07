f0e933f861d7cd96de33c966ff45e573416bf38e
weitanai <987346185@qq.com>
2023-06-07 21:28:19 +0800

    chore: remove vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import UnoCSS from "unocss/vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), AutoImport(), Components(), UnoCSS(), vercel()],
+  plugins: [vue(), AutoImport(), Components(), UnoCSS()],
   server: {
     port: 3000,
     open: true,
